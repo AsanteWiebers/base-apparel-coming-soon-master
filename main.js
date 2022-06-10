610b00eac9996ccbc39d84d74d859677b8d10d67
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-06-10 23:31:23 +0200

    desktop updates

---
+++ main.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+// Home page
+
+ScrollReveal().reveal('.header', {
+    delay: 0.1,
+    origin: 'right'
+});
